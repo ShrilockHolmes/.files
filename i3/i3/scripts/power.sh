2f8e9b4af47df73861d470399090ccd52644923c
Amith S <amiths12@outlook.com>
2024-01-07 11:38:51 +0530

    hi

--- i3/i3/scripts/power.sh
+++ i3/i3/scripts/power.sh
@@ -6,7 +6,7 @@ case "$chosen" in
 	"  Shutdown") poweroff ;;
 	"  Restart") reboot ;;
   "💤 Sleep") systemctl suspend-then-hibernate ;;
-	"  Log Out") i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit';;
+	"  Log Out") i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit' | bspc quit;;
 	"  Lock") betterlockscreen -l ;;
 	*) exit 1 ;;
 esac
