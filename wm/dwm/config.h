eb15a73e5a53c5b490e99523b07ebe73d81ff3a4
ShrilockHolmes <amtihs12@outlook.com>
2024-12-07 12:14:44 +0530

    me

--- wm/dwm/config.h
+++ wm/dwm/config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include "movestack.c"
 /* appearance */
 static const unsigned int refresh_rate =
     60; /* matches dwm's mouse event processing to your monitor's refresh rate
@@ -73,17 +73,18 @@ static const int resizehints =
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
-#include "fibonacci.c"
+// #include "fibonacci.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"", tile}, /* first entry is default */
     //{"D", deck},
-    {"(@)", spiral},
+    //   {"(@)", spiral},
     {"", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},
+    //  {"[M]", monocle},
 };
 
 /* key definitions */
+#define alt Mod1Mask
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                      \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
@@ -125,34 +126,35 @@ static Key keys[] = {
     //= -1 } },
     {MODKEY | ShiftMask, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY | ShiftMask, XK_y, setlayout, {.v = &layouts[1]}},
-    // { MODKEY|ShiftMask,             XK_j,                      movestack, {.i
-    // = +1 } }, { MODKEY|ShiftMask,             XK_l, movestack,      {.i = +1
-    // } }, { MODKEY|ShiftMask,             XK_h, movestack,      {.i = -1 } },
-    //{ MODKEY|ShiftMask,             XK_k,                      movestack, {.i
-    //= -1 } },
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    //{ MODKEY,                       XK_d,                      incnmaster, {.i
-    //= -1 } },
-    {MODKEY | ShiftMask, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY | ShiftMask, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY | ShiftMask, XK_k, setcfact, {.f = +0.25}},
-    {MODKEY | ShiftMask, XK_j, setcfact, {.f = -0.25}},
-    {MODKEY | ShiftMask, XK_o, setcfact, {.f = 0.00}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_q, killclient, {0}},
-    //{ MODKEY,                       XK_t,                      setlayout, {.v
-    //= &layouts[0]} }, { MODKEY,                       XK_v, setlayout, {.v =
-    //&layouts[1]} }, { MODKEY,                       XK_f, setfullscreen, {0}
-    //},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    //{ MODKEY,                       XK_0,                      view, {.ui = ~0
-    //} }, { MODKEY,                       XK_comma,                  focusmon,
-    //{.i = -1 } }, { MODKEY,                       XK_period, focusmon, {.i =
-    //+1 } }, { MODKEY|ShiftMask,             XK_comma,                  tagmon,
-    //{.i = -1 } }, { MODKEY|ShiftMask,             XK_period, tagmon, {.i = +1
-    //} },
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4)};
+    {alt | ShiftMask, XK_h, movestack, {.i = +1}},
+    {alt | ShiftMask, XK_l, movestack,{.i = +1} }, 
+ // { MODKEY|ShiftMask,             XK_h, movestack,      {.i =
+           // -1 } },
+           //{ MODKEY|ShiftMask,             XK_k, movestack, {.i = -1 } },
+           {MODKEY, XK_i, incnmaster, {.i = +1}},
+      //{ MODKEY,                       XK_d,                      incnmaster,
+      //{.i = -1 } },
+      {MODKEY | ShiftMask, XK_h, setmfact, {.f = -0.05}},
+      {MODKEY | ShiftMask, XK_l, setmfact, {.f = +0.05}},
+      {MODKEY | ShiftMask, XK_k, setcfact, {.f = +0.25}},
+      {MODKEY | ShiftMask, XK_j, setcfact, {.f = -0.25}},
+      {MODKEY | ShiftMask, XK_o, setcfact, {.f = 0.00}},
+      {MODKEY, XK_Return, zoom, {0}},
+      {MODKEY, XK_q, killclient, {0}},
+      //{ MODKEY,                       XK_t,                      setlayout,
+      //{.v = &layouts[0]} }, { MODKEY,                       XK_v, setlayout,
+      //{.v = &layouts[1]} }, { MODKEY,                       XK_f,
+      //setfullscreen, {0}
+      //},
+      {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+      //{ MODKEY,                       XK_0,                      view, {.ui =
+      //~0 } }, { MODKEY,                       XK_comma, focusmon,
+      //{.i = -1 } }, { MODKEY,                       XK_period, focusmon, {.i =
+      //+1 } }, { MODKEY|ShiftMask,             XK_comma, tagmon,
+      //{.i = -1 } }, { MODKEY|ShiftMask,             XK_period, tagmon, {.i =
+      //+1 } },
+      TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+          TAGKEYS(XK_5, 4)};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
@@ -162,7 +164,7 @@ static Button buttons[] = {
     {ClkTagBar, MODKEY, Button1, tag, {0}},
     {ClkTagBar, MODKEY, Button3, toggletag, {0}},
     //{ ClkClientWin,         MODKEY,         Button1,        moveorplace, {.i =
-    //2} },
+    // 2} },
     {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
     {ClkTagBar, 0, Button1, view, {0}},
     {ClkTagBar, 0, Button3, toggleview, {0}},
